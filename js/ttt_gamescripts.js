5429899782e302f74b20d874abbda2523ea85b97
Evan Pavlica <evan@xicreative.net>
2013-09-26 11:19:11 -0700

    Add js to prevent playets from overwriting cells; clean up meta tag.

--- js/ttt_gamescripts.js
+++ js/ttt_gamescripts.js
@@ -28,11 +28,19 @@ var playCount = 1;
 
 //
 function playerTurn(id) {
-	if(isEven(playCount)) {
-		document.getElementById(id).innerHTML = "<img class='o' src='resources/images/xo_sprites.png'>";
+	var cell = document.getElementById(id).innerHTML;
+	
+	if(cell.length == 0){
+		if(isEven(playCount)) {
+			document.getElementById(id).innerHTML = "<img class='o' src='resources/images/xo_sprites.png'>";
+			playCount += 1;
+		}
+		else if(isEven(playCount) == false) {
+			document.getElementById(id).innerHTML = "<img class='x' src='resources/images/xo_sprites.png'>";
+			playCount += 1;
+		}
 	}
 	else {
-		document.getElementById(id).innerHTML = "<img class='x' src='resources/images/xo_sprites.png'>";
+			alert("That cell is already full! Please choose another.");
 	}
-	playCount += 1;
 }
